9576e527b59d297e3a3ab397bcb9a52f22c1f670
Felddy <markf@geekpad.com>
2019-08-06 18:41:05 -0400

    Calm shellcheck

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -4,7 +4,7 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
-if [ $1 = "--version" ]; then
+if [ "$1" = "--version" ]; then
   echo 0.0.1
   exit 0
 fi
@@ -12,12 +12,12 @@ fi
 ACME_CONFIG_ROOT=/etc/letsencrypt
 
 echo "Syncing certbot configs from ${BUCKET_NAME}"
-AWS_PROFILE=${BUCKET_PROFILE} aws s3 sync s3://${BUCKET_NAME} ${ACME_CONFIG_ROOT}
+AWS_PROFILE=${BUCKET_PROFILE} aws s3 sync "s3://${BUCKET_NAME}" ${ACME_CONFIG_ROOT}
 
 ./rebuild-symlinks.py --log-level warning ${ACME_CONFIG_ROOT}
 
-# shellcheck disable=SC2048
+# shellcheck disable=SC2048,SC2086
 AWS_PROFILE=${DNS_PROFILE} certbot $*
 
 echo "Syncing certbot configs to ${BUCKET_NAME}"
-AWS_PROFILE=${BUCKET_PROFILE} aws s3 sync --delete ${ACME_CONFIG_ROOT} s3://${BUCKET_NAME}
+AWS_PROFILE=${BUCKET_PROFILE} aws s3 sync --delete ${ACME_CONFIG_ROOT} "s3://${BUCKET_NAME}"
