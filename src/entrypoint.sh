5f291d3908fb241e7ae9147b803a4fccd99f2149
Felddy <markf@geekpad.com>
2019-08-15 14:06:23 -0400

    Clarify certbot execution message. This will allow one to copy and paste the line for testing.

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -25,7 +25,7 @@ AWS_PROFILE=${BUCKET_PROFILE} aws s3 sync --no-progress "s3://${BUCKET_NAME}" \
 echo "Rebuilding symlinks in ${ACME_CONFIG_ROOT}"
 ./rebuild-symlinks.py --log-level warning ${ACME_CONFIG_ROOT}
 
-echo "Running certbot with arguments --dns-route53 $*"
+echo "Running: certbot --dns-route53 $*"
 # shellcheck disable=SC2048,SC2086
 AWS_PROFILE=${DNS_PROFILE} certbot --dns-route53 $*
 
