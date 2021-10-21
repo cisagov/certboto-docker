62f818e045b075cc6dc0c73883c729095a744cdc
Felddy <markf@geekpad.com>
2021-10-21 16:41:54 -0400

    Add flag to allow dropping into a shell
    This required rewriting / adding proper argument parsing.

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -12,17 +12,37 @@ set -o errexit
 # shellcheck disable=SC2039,SC3040
 set -o pipefail
 
-if [ "$1" = "--version" ]; then
-  awk '{print $3}' < version.txt | tr -d \"
-  certbot --version
-  exit 0
-fi
+certbot_args=""
+use_route53=true
+drop_to_shell=false
 
-# Allow users to get help without bucket syncing
-if [ "$1" = "--help" ] || [ "$1" = "-h" ]; then
-  certbot --help
-  exit $?
-fi
+# Parse command line arguments
+while [ $# -gt 0 ]; do
+  case "$1" in
+    --version)
+      awk '{print $3}' < version.txt | tr -d \"
+      certbot --version
+      exit 0
+      ;;
+    -h | --help) # Allow users to get help without bucket syncing
+      certbot --help
+      exit $?
+      ;;
+    --no-dns-route53)
+      echo "Route53 DNS challenge disabled by --no-dns-route53 flag"
+      use_route53=false
+      shift
+      ;;
+    --shell)
+      drop_to_shell=true
+      shift
+      ;;
+    *) # add to certbot_args
+      certbot_args="$certbot_args $1"
+      shift
+      ;;
+  esac
+done
 
 ACME_CONFIG_ROOT=/etc/letsencrypt
 
@@ -33,18 +53,22 @@ AWS_PROFILE=${BUCKET_PROFILE} aws s3 sync --no-progress "s3://${BUCKET_NAME}" \
 echo "Rebuilding symlinks in ${ACME_CONFIG_ROOT}"
 ./rebuild-symlinks.py --log-level warning ${ACME_CONFIG_ROOT}
 
-# First argument flag --no-dns-route53 disables default use of --dns-route53
-if [ "$1" = "--no-dns-route53" ]; then
-  shift
-  echo "Route53 DNS challenge disabled by --no-dns-route53 flag"
-else
+if [ $use_route53 = true ]; then
   # Add the --dns-route53 argument to the start of our args
-  set -- --dns-route53 "$*"
+  certbot_args="--dns-route53 $certbot_args"
 fi
 
-echo "Running: certbot $*"
-# shellcheck disable=SC2048,SC2086
-AWS_PROFILE=${DNS_PROFILE} certbot $*
+if [ $drop_to_shell = true ]; then
+  echo "Starting a shell as requested by argument --shell"
+  echo "Certbot configs will be synchronized upon shell exit."
+  echo "Would have run command: AWS_PROFILE=${DNS_PROFILE} certbot ${certbot_args}"
+  /bin/sh
+  echo
+else
+  echo "Running: certbot $certbot_args"
+  # shellcheck disable=SC2048,SC2086
+  AWS_PROFILE=${DNS_PROFILE} certbot $certbot_args
+fi
 
 echo "Syncing certbot configs to ${BUCKET_NAME}"
 AWS_PROFILE=${BUCKET_PROFILE} aws s3 sync --delete ${ACME_CONFIG_ROOT} "s3://${BUCKET_NAME}"
